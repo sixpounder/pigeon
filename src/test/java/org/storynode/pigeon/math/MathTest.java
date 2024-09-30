c94e217bab3429ce709a4c5bac74e03f77978a9d
Andrea Coronese <sixpounder@protonmail.com>
2024-09-30 11:27:56 +0200

    Add Math.clamp

---
+++ src/test/java/org/storynode/pigeon/math/MathTest.java
@@ -0,0 +1,35 @@
+package org.storynode.pigeon.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MathTest {
+
+    @Test
+    void clamp_int() {
+        assertThat(Math.clamp(23, 0, 100)).as("Clamped int").isEqualTo(23);
+        assertThat(Math.clamp(-12, 0, 100)).as("Clamped int").isEqualTo(0);
+        assertThat(Math.clamp(124, 0, 100)).as("Clamped int").isEqualTo(100);
+        assertThat(Math.clamp(0, 0, 100)).as("Clamped int").isEqualTo(0);
+        assertThat(Math.clamp(100, 0, 100)).as("Clamped int").isEqualTo(100);
+    }
+
+    @Test
+    void clamp_double() {
+        assertThat(Math.clamp(23d, 0d, 100d)).as("Clamped int").isEqualTo(23);
+        assertThat(Math.clamp(-12d, 0d, 100d)).as("Clamped int").isEqualTo(0);
+        assertThat(Math.clamp(124d, 0d, 100d)).as("Clamped int").isEqualTo(100);
+        assertThat(Math.clamp(0d, 0d, 100d)).as("Clamped int").isEqualTo(0);
+        assertThat(Math.clamp(100d, 0d, 100d)).as("Clamped int").isEqualTo(100);
+    }
+
+    @Test
+    void clamp_float() {
+        assertThat(Math.clamp(23.0, 0.0, 100.2)).as("Clamped int").isEqualTo(23.0);
+        assertThat(Math.clamp(-12.12, 0.98, 100.12)).as("Clamped int").isEqualTo(0.98);
+        assertThat(Math.clamp(124.1, 0.0, 100.1)).as("Clamped int").isEqualTo(100.1);
+        assertThat(Math.clamp(0.0, 0.0, 100.0)).as("Clamped int").isEqualTo(0.0);
+        assertThat(Math.clamp(100.0, 0.1, 100.0)).as("Clamped int").isEqualTo(100.0);
+    }
+}
